dd42f4e3a4c8fc4c232dd8aa5aaa215910f88204
Yroslavadudko <dudkomykola@icloud.com>
2023-07-22 23:17:02 +0300

    Auto

--- index.js
+++ index.js
@@ -47,3 +47,50 @@
 
 // 2+2
 // console.log("🚀 ~ 2:", 2) //?
+
+
+// const apples = '47';
+// const greapes = 135;
+// const total = Number(apples) + Number(greapes);
+// console.log('total', total);
+
+// const diff = greapes-apples;
+// console.log("diff", diff)
+
+// const diff1 = apples - greapes;
+// console.log("diff1", diff1);
+
+// let students = 100;
+// // students = students +50;
+// students += 50;
+// console.log('students:', students);
+
+//Task-1/27
+// const productName = 'Droid';
+// const pricePerItem = 2000;
+// console.log(productName);
+// console.log(pricePerItem);
+
+// //Task-2/27
+// let productName = "Droid";
+// productName = "Repair droid";
+// let pricePerItem = 2000;
+// pricePerItem = 2000 + 1500;
+// console.log('productName:' , productName);
+// console.log('pricePerItem:' , pricePerItem);
+
+//Task-3/27
+// const topSpeed = 160;
+// console.log("topSpeed:", topSpeed);
+
+// const distance = 617.54;
+// console.log("distance:", distance);
+
+// const login = "mango935";
+// console.log("login:", login);
+
+// const isOnline = true;
+// console.log("isOnline:", isOnline);
+
+// const isAdmin = false;
+// console.log("isAdmin:", isAdmin);
