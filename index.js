8fc40f979ddc002fe6b97dff92b62393a3303f09
Yroslavadudko <dudkomykola@icloud.com>
2023-07-22 18:49:54 +0300

    1-2 module
    трошки практики

---
+++ index.js
@@ -0,0 +1,49 @@
+// const user = "Mango"
+
+// let age = 5;
+// age = 25;
+// console.log('age', age)
+
+// const quantity = age;
+// console.log('quantity', quantity)
+
+// age = 50;
+// console.log('age', age)
+// console.log("quantity", quantity);
+
+
+// const valueB = "random string";
+// console.log(Number (valueB)); //'NaN'
+// console.log(typeof Number (valueB));
+// isNaN(13);
+// console.log('isNaN(13):', isNaN(undefined)); //undefined точно не число
+
+// 5>4
+// console.log("🚀 ~ 5>4:", 5>4)
+
+// 5>=4
+// console.log("🚀 ~ 5>=4:", 5>=4)
+
+// 5>=6
+// console.log("🚀 ~ 5>=6:", 5>=6)
+
+// 5<7
+// console.log("🚀 ~ 7:", 7)
+
+// 5<=5
+// console.log("🚀 ~ 5:", 5)
+
+// 6===7
+// console.log("🚀 ~ 7:", 7)
+
+// 5/2
+// console.log("🚀 ~ 5/2:", 5/2)
+
+// 6*6
+// console.log("🚀 ~ 6*6:", 6*6)
+
+// 1236-477
+// console.log("🚀 ~ 1236-477:", 1236-477)
+
+// 2+2
+// console.log("🚀 ~ 2:", 2) //?
